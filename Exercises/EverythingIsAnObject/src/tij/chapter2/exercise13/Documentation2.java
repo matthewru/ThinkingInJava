a6ed01f814f334c4b9c4ceb5ad2cb330013bf5b0
matthewru <matthewru2005@gmail.com>
2018-08-28 20:49:46 -0500

    Javadoc exercise done

---
+++ Exercises/EverythingIsAnObject/src/tij/chapter2/exercise13/Documentation2.java
@@ -0,0 +1,15 @@
+package tij.chapter2.exercise13;
+
+//: object/Documentation2.java
+/**
+* <pre>
+* System.out.println(new Date());
+* </pre>
+*/
+///:~
+public class Documentation2 {
+/** A field comment */
+public int i;
+/** A method comment */
+public void f() {}
+} ///:~ 
---
+++ Exercises/EverythingIsAnObject/src/tij/chapter2/exercise13/Documentation3.java
@@ -0,0 +1,18 @@
+package tij.chapter2.exercise13;
+
+//: object/Documentation3.java
+/**
+* You can <em>even</em> insert a list:
+* <ol>
+* <li> Item one
+* <li> Item two
+* <li> Item three
+* </ol>
+*/
+///:~ 
+public class Documentation3 {
+/** A field comment */
+public int i;
+/** A method comment */
+public void f() {}
+} ///:~ 
